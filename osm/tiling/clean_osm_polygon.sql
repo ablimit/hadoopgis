e173b37a66c4483b5d110534504dca3704a71396
Ablimit Aji <aaji@mathcs.emory.edu>
2013-02-07 00:21:12 -0500

    clean polygon SQL

--- osm/tiling/clean_osm_polygon.sql
+++ osm/tiling/clean_osm_polygon.sql
@@ -1,3 +1,8 @@
 
-CREATE TABLE  ( osm_id  bigint, z_order real, area    real, );
+CREATE TABLE clean_polygon (osm_id  bigint, z_order real, area  real, );
+
+SELECT AddGeometryColumn('clean_polygon','way', 4326, 'POLYGON', 2 ) ;
+
+SELECT osm_id,z_order,area,way INTO clean_osm FROM planet_osm_polygon ; 
+
 
