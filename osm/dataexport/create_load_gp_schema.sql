d7eeec1d2c851f789480e5df8fc0dd4a9b595848
Ablimit Aji <aaji@mathcs.emory.edu>
2013-02-10 14:00:02 -0500

    dump and import scripts

---
+++ osm/dataexport/create_load_gp_schema.sql
@@ -0,0 +1,23 @@
+
+-- tile level 
+BEGIN ;
+
+CREATE TABLE osm_polygon ( 
+    id		integer PRIMARY KEY,
+    tilename	varchar(20),
+    osm_id      bigint,
+    z_order        integer) 
+DISTRIBUTED BY (tilename);
+
+SELECT AddGeometryColumn('public','osm_polygon', 'way', 4326, 'POLYGON', 2);
+
+COPY osm_polygon FROM '/data2/ablimit/Data/spatialdata/osmout/clean_osm.dat' WITH HEADER DELIMITER '|' ;
+
+COMMIT ;
+
+CREATE INDEX osm_polygon_sp_idx ON osm_polygon USING GIST (way);
+
+CREATE INDEX osm_polygon_fidx ON osm_polygon (tilename);
+
+VACUUM VERBOSE ANALYZE osm_polygon;
+
