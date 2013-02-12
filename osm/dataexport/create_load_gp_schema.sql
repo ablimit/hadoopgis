d747e970dc7c24bc94998e7b1d39a3afc3a71e59
ablimit <ablimit@gmail.com>
2013-02-12 14:17:54 -0500

    invalid commit action eliminated

--- osm/dataexport/create_load_gp_schema.sql
+++ osm/dataexport/create_load_gp_schema.sql
@@ -2,7 +2,7 @@
 -- tile level 
 BEGIN ;
 
-DROP TABLE  osm_polygon_europe; 
+-- DROP TABLE  osm_polygon_europe; 
 
 CREATE TABLE osm_polygon_europe ( 
     id		integer,
@@ -13,13 +13,12 @@ DISTRIBUTED BY (tilename);
 
 SELECT AddGeometryColumn('public','osm_polygon_europe', 'way', -1, 'POLYGON', 2);
 
-COPY osm_polygon_europe FROM '/data2/ablimit/Data/spatialdata/osmout/osm_polygon_europe.dat' WITH HEADER DELIMITER '|' ;
+COPY osm_polygon_europe FROM '/data2/ablimit/Data/spatialdata/osmout/europe_osm_polygon.dat' WITH HEADER DELIMITER '|' ;
 
 -- COPY osm_polygon_europe FROM '/tmp/temp.dat' WITH DELIMITER AS '|' CSV HEADER ;
 
 COMMIT ;
 
-BEGIN;
 
 CREATE INDEX osm_polygon_europe_sp_idx ON osm_polygon_europe USING GIST (way);
 
@@ -27,4 +26,3 @@ CREATE INDEX osm_polygon_europe_fidx ON osm_polygon_europe (tilename);
 
 VACUUM VERBOSE ANALYZE osm_polygon_europe;
 
-COMMIT ;
