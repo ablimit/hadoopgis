eb0247809eb7b3977e6fb76cabef9b3e597ff75d
ablimit <ablimit@gmail.com>
2013-02-15 16:47:03 -0500

    queries for OSM

---
+++ benchmark/query/osm/aggregation.sql
@@ -0,0 +1,50 @@
+-- feature aggreagation
+
+-- For each human marked region (polygon), count the number of nuclei in that region and the average features of them.
+
+-- a) flat feature aggregation 
+SELECT 	count(m.markup_id) NUM_NUCLEI, 
+    avg(f.AREA) AVG_AREA,
+    avg(f.eccentricity) AVG_ECC,
+    avg(f.PERIMETER) AVG_PERIMETER
+FROM   	PAIS.MARKUP_POLYGON m, 
+        PAIS.MARKUP_POLYGON_HUMAN r,
+        PAIS.CALCULATION_FLAT f
+WHERE  	m.pais_uid ='gbm1.1_40x_20x_NS-MORPH_1' AND 
+        f.pais_uid = m.pais_uid AND 
+        r.pais_uid = 'gbm1.1_40x_40x_RG-HUMAN_1' AND 
+        f.pais_uid = r.pais_uid AND 
+        f.markup_id = m.markup_id AND 
+        ST_Contains(r.polygon, m.polygon) = TRUE;
+
+
+-- b) spatial feature aggregation 
+SELECT 	count(polygon) NUM_NUCLEI, 
+    avg(f.AREA) AVG_AREA,
+    avg(f.eccentricity) AVG_ECC,
+    avg(f.PERIMETER) AVG_PERIMETER,
+    S
+FROM   	pais.markup_polygon  
+WHERE  	pais_uid = 'gbm1.1_40x_20x_NS-MORPH_1'  AND ST_Area(polygon) > 10 AND 
+ST_Contains( ST_PolygonFromText('POLYGON((22528 8192,67584 8192,67584 24576,22528 24576,22528 8192))', 100), polygon ) = TRUE ;
+
+SELECT 	count(m.markup_id) NUM_NUCLEI, 
+    avg(f.AREA) AVG_AREA,
+    avg(f.eccentricity) AVG_ECC,
+    avg(f.PERIMETER) AVG_PERIMETER
+FROM   	PAIS.MARKUP_POLYGON m, 
+        PAIS.MARKUP_POLYGON_HUMAN r,
+        PAIS.CALCULATION_FLAT f
+WHERE  	m.pais_uid ='gbm1.1_40x_20x_NS-MORPH_1' AND 
+        f.pais_uid = m.pais_uid AND 
+        r.pais_uid = 'gbm1.1_40x_40x_RG-HUMAN_1' AND 
+        f.pais_uid = r.pais_uid AND 
+        f.markup_id = m.markup_id AND 
+        ST_Contains(r.polygon, m.polygon) = TRUE;
+
+SELECT 	polygon
+FROM   	pais.markup_polygon  
+WHERE  	pais_uid = 'gbm1.1_40x_20x_NS-MORPH_1'  AND ST_Area(polygon) > 10 ;
+average nuclei area, cell area
+
+
---
+++ benchmark/query/osm/containment.sql
@@ -0,0 +1,47 @@
+-- Type 1: Containment Query
+
+-- Selection with field filtering (small region)
+SELECT 	way 
+FROM   	osm_planet_polygon 
+WHERE  	pais_uid = 'gbm1.1_40x_20x_NS-MORPH_1'  AND tilename ='gbm1.1-0000040960-0000040960' AND
+ST_Contains( ST_PolygonFromText('POLYGON((40960 40960, 41984 40960,  41984 41984, 40960 41984, 40960 40960))', 100), polygon ) = TRUE ;
+
+-- Selection without field filtering (small region)
+SELECT 	polygon
+FROM   	pais.markup_polygon  
+WHERE  ST_Contains( ST_PolygonFromText('POLYGON((40960 40960, 41984 40960,  41984 41984, 40960 41984, 40960 40960))', 100), polygon ) = TRUE ;
+
+
+-- Selection with field filtering (large region)
+SELECT 	polygon
+FROM   	pais.markup_polygon  
+WHERE  	pais_uid = 'gbm1.1_40x_20x_NS-MORPH_1'  AND 
+ST_Contains( ST_PolygonFromText('POLYGON((22528 8192,67584 8192,67584 24576,22528 24576,22528 8192))', 100), polygon ) = TRUE ;
+
+-- Selection with field filtering 
+SELECT 	polygon
+FROM   	pais.markup_polygon  
+WHERE  	pais_uid = 'gbm1.1_40x_20x_NS-MORPH_1'  AND ST_Area(polygon) > 10 AND 
+ST_Contains( ST_PolygonFromText('POLYGON((22528 8192,67584 8192,67584 24576,22528 24576,22528 8192))', 100), polygon ) = TRUE ;
+
+-- Selection with field filtering 
+SELECT 	polygon
+FROM   	pais.markup_polygon  
+WHERE  	pais_uid = 'gbm1.1_40x_20x_NS-MORPH_1'  AND ST_Area(polygon) > 10 ;
+
+-- b) Given a human-marked region in a single tile, which is stored in the MARKUP_POLYGON_HUMAN table.
+
+-- SELECT 	p.polygon
+-- FROM   		pais.markup_polygon p, pais.markup_polygon_human h  
+-- WHERE  	p.pais_uid = 'gbm1.1_40x_20x_NS-MORPH_1'  AND  p.tilename ='gbm1.1-0000040960-0000040960' AND 
+--	h.pais_uid = p.pais_uid AND h.tilename = p.tilename AND ST_Contains( h.polygon, p.polygon ) = TRUE ;
+
+
+-- c) Given a human-marked region among multiple tiles, which is stored in the MARKUP_POLYGON_HUMAN table.
+
+
+-- SELECT p.polygon
+-- FROM 		pais.markup_polygon p, pais.markup_polygon_human h
+-- WHERE 	p.pais_uid = 'gbm1.1_40x_20x_NS-MORPH_1' AND p.pais_uid = h.pais_uid AND ST_Contains(h.polygon, p.polygon) = TRUE ;
+
+
