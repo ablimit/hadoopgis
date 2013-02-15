0308fa111b46150ca4880c622c1b913ae2654d7b
Ablimit Aji <aaji@mathcs.emory.edu>
2013-02-15 18:54:56 -0500

    @

--- benchmark/query/osm/aggregation.sql
+++ benchmark/query/osm/aggregation.sql
@@ -19,3 +19,4 @@ SELECT 	count(polygon) NUM_NUCLEI,
 FROM   	pais.markup_polygon  
 WHERE  	pais_uid = 'gbm1.1_40x_20x_NS-MORPH_1'  AND ST_Area(polygon) > 10 AND 
 ST_Contains( ST_PolygonFromText('POLYGON((22528 8192,67584 8192,67584 24576,22528 24576,22528 8192))', 100), polygon ) = TRUE ;
+
