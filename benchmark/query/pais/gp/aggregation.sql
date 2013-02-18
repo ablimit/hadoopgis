ec09e4eabc02631e8a9a2e681779cf95569900e6
ablimit <ablimit@gmail.com>
2013-02-18 15:22:08 -0500

    @

--- benchmark/query/pais/gp/aggregation.sql
+++ benchmark/query/pais/gp/aggregation.sql
@@ -3,23 +3,24 @@
 -- For each human marked region (polygon), count the number of nuclei in that region and the average features of them.
 
 -- a) flat feature aggregation  with join
-/* 
-SELECT 	count(m.markup_id) NUM_NUCLEI, 
-    avg(f.AREA) AVG_AREA,
-    avg(f.eccentricity) AVG_ECC,
-    avg(f.PERIMETER) AVG_PERIMETER
-FROM   	PAIS.MARKUP_POLYGON m, 
-        PAIS.MARKUP_POLYGON_HUMAN r,
-        PAIS.CALCULATION_FLAT f
-WHERE  	m.pais_uid ='gbm1.1_40x_20x_NS-MORPH_1' AND 
-        f.pais_uid = m.pais_uid AND 
-        r.pais_uid = 'gbm1.1_40x_40x_RG-HUMAN_1' AND 
-        f.pais_uid = r.pais_uid AND 
-        f.markup_id = m.markup_id AND 
-        ST_Contains(r.polygon, m.polygon) = TRUE;
-*/
+ 
+-- SELECT 	count(m.markup_id) NUM_NUCLEI, 
+--    avg(f.AREA) AVG_AREA,
+--    avg(f.eccentricity) AVG_ECC,
+--    avg(f.PERIMETER) AVG_PERIMETER
+-- FROM   	PAIS.MARKUP_POLYGON m, 
+--        PAIS.MARKUP_POLYGON_HUMAN r,
+--        PAIS.CALCULATION_FLAT f
+-- WHERE  	m.pais_uid ='gbm1.1_40x_20x_NS-MORPH_1' AND 
+--	  f.pais_uid = m.pais_uid AND 
+--        r.pais_uid = 'gbm1.1_40x_40x_RG-HUMAN_1' AND 
+--        f.pais_uid = r.pais_uid AND 
+--        f.markup_id = m.markup_id AND 
+--        ST_Contains(r.polygon, m.polygon) = TRUE;
 
+\timing on 
 -- b) spatial feature aggregation (single tile)
+
 SELECT 
     ST_Area(polygon) AS AREA,
     ST_Centroid(polygon) AS CENTROID,
@@ -55,4 +56,6 @@ SELECT
     ST_Perimeter(polygon) AS PERIMETER
 FROM	markup_polygon 
 WHERE ST_Area(polygon) > 125.0;
+
+\timing off
 
