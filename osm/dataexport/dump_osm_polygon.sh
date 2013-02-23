77c71e02d54a8667cd9b346ec3bb9abb3ba20425
Ablimit Aji <aaji@mathcs.emory.edu>
2013-02-22 22:05:17 -0500

    --

--- osm/dataexport/dump_osm_polygon.sh
+++ osm/dataexport/dump_osm_polygon.sh
@@ -1,6 +1,8 @@
 #! /bin/bash
 
-psql --dbname=osm --no-align --pset=footer=off --command="SELECT id, tilenameb, osm_id, z_order, ST_AsText(way) AS way FROM planet_osm_polygon ;" --output=/data2/ablimit/Data/spatialdata/osmout/planet_osm_polygon.dat
+tilecolumn=tilenameb
 
-psql --dbname=osmeu --no-align --pset=footer=off --command="SELECT id, tilenameb, osm_id, z_order, ST_AsText(way) AS way FROM planet_osm_polygon ;" --output=/data2/ablimit/Data/spatialdata/osmout/europe_osm_polygon.dat
+psql --dbname=osm --no-align --pset=footer=off --command="SELECT id, ${tilecolumn}, osm_id, z_order, ST_AsText(way) AS way FROM planet_osm_polygon ;" --output=/data2/ablimit/Data/spatialdata/osmout/osm_polygon_planet.dat
+
+psql --dbname=osmeu --no-align --pset=footer=off --command="SELECT id, ${tilecolumn}, osm_id, z_order, ST_AsText(way) AS way FROM planet_osm_polygon ;" --output=/data2/ablimit/Data/spatialdata/osmout/osm_polygon_europe.dat
 
