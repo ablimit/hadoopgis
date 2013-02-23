6b6c577658565c17e8e9e714e7efacf5136b17ef
Ablimit Aji <aaji@mathcs.emory.edu>
2013-02-22 21:52:11 -0500

    @

--- osm/tiling/gen_tilename_postgres.sh
+++ osm/tiling/gen_tilename_postgres.sh
@@ -1,10 +1,6 @@
 #! /bin/bash
 
 
-psql -d osm -c "ALTER TABLE planet_osm_polygon ADD COLUMN 'tilenameb' varchar(32);" 
+psql -d osm -c "BEGIN; ALTER TABLE planet_osm_polygon ADD COLUMN tilenameb varchar(32); UPDATE planet_osm_polygon SET tilenameb = tileb.tilename FROM tileb WHERE ST_Contains(tileb.mbb,planet_osm_polygon.way) = TRUE;  COMMIT; "
 
-psql -d osm -c " UPDATE planet_osm_polygon SET tilenameb = tileb.tilename FROM tileb WHERE ST_Contains(tileb.mbb,osm_polygon_planet.way) = TRUE;"
-
-psql -d osmeu -c "ALTER TABLE planet_osm_polygon ADD COLUMN 'tilenameb' varchar(32);" 
-
-psql -d osmeu -c " UPDATE planet_osm_polygon SET tilenameb = tileb.tilename FROM tileb WHERE ST_Contains(tileb.mbb,osm_polygon_planet.way) = TRUE;"
+psql -d osmeu -c "BEGIN; ALTER TABLE planet_osm_polygon ADD COLUMN tilenameb varchar(32); UPDATE planet_osm_polygon SET tilenameb = tileb.tilename FROM tileb WHERE ST_Contains(tileb.mbb,planet_osm_polygon.way) = TRUE; COMMIT;"
