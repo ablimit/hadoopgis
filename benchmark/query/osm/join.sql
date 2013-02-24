5b3767807e92f327de008141ae0b1f253d6242ab
ablimit <ablimit@gmail.com>
2013-02-24 14:13:35 -0500

    sytactic error in join.sql for OSM is fixed.

--- benchmark/query/osm/join.sql
+++ benchmark/query/osm/join.sql
@@ -1,12 +1,13 @@
-﻿-- spatial join with predicate of polygon intersection
+-- spatial join with predicate of polygon intersection
 
 -- a) two way join
 SELECT count(*) from (
 
     SELECT A.id, A.tilename, ST_Area(ST_Intersection(A.way, B.way)) / ST_Area(ST_Union( A.way, B.way))  AS a_ratio,
     ST_DISTANCE(ST_Centroid(A.way), ST_Centroid(B.way)) AS centroid_distance
-    FROM osm.osm_polygon_planet A, osm.osm_polygon_europe B
+    FROM osm.osm_polygon_planet_fourxfour A, osm.osm_polygon_europe_fourxfour B
     WHERE  A.tilename = B.tilename AND length(A.tilename) > 2 AND 
-	ST_Equals(A.polygon, B.polygon) = FALSE;
+	ST_Equals(A.way, B.way) = FALSE;
 ) AS temp ; 
+
 
