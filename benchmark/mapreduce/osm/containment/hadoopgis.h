2eca590c7be2e63803f1a0ec4dfa5306cf6e3beb
Ablimit Aji <aaji@mathcs.emory.edu>
2013-03-03 14:41:24 -0500

    Containment query1 query2 query3 are done. resque is using geos and boost together

--- benchmark/mapreduce/osm/containment/hadoopgis.h
+++ benchmark/mapreduce/osm/containment/hadoopgis.h
@@ -4,24 +4,35 @@
 #include <errno.h>
 #include <sys/stat.h>
 #include <sys/types.h>
-
-#include <iostream>
 #include <string.h>
-#include <string>
 
+// std 
+#include <iostream>
+#include <string>
 #include <vector>
 #include <map>
 
+// boost 
 #include <boost/foreach.hpp>
 #include <boost/algorithm/string.hpp>
 #include <boost/geometry.hpp>
 #include <boost/geometry/geometry.hpp>
+#include <boost/geometry/algorithms/covered_by.hpp>
 #include <boost/geometry/geometries/point_xy.hpp>
 #include <boost/geometry/geometries/polygon.hpp>
 #include <boost/geometry/geometries/box.hpp>
 #include <boost/geometry/domains/gis/io/wkt/wkt.hpp>
 
+// spatial index 
 #include <spatialindex/SpatialIndex.h>
+
+// geos 
+#include <geos/geom/PrecisionModel.h>
+#include <geos/geom/GeometryFactory.h>
+#include <geos/geom/Geometry.h>
+#include <geos/io/WKTReader.h>
+
+#define PAIS_SRID 4326
 
 #define FillFactor 0.7
 #define IndexCapacity 100
@@ -36,7 +47,13 @@
 #define OSM_POLYGON 4
 
 
+
 using namespace std;
+
+using namespace geos;
+using namespace geos::io;
+using namespace geos::geom;
+
 using namespace SpatialIndex;
 
 using boost::lexical_cast;
@@ -48,12 +65,12 @@ typedef boost::geometry::model::box<point> box;
 
 typedef map<string,map<int,vector<polygon> > > polygonmap;
 
-const string bar= "|";
-const string tab = "\t";
-const string comma = ",";
-const string space = " ";
+const string UNDERSCORE = "_";
+const string BAR= "|";
+const string TAB = "\t";
+const string COMMA = ",";
+const string SPACE = " ";
 
 const string shapebegin = "POLYGON((";
 const string shapeend = "))";
-
 
