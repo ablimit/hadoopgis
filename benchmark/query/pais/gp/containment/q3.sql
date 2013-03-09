802eff25bfc901c48947b7fa0f9826932d518905
Ablimit Aji <aaji@mathcs.emory.edu>
2013-03-08 23:49:57 -0500

    selection region for q3 is changed

--- benchmark/query/pais/gp/containment/q3.sql
+++ benchmark/query/pais/gp/containment/q3.sql
@@ -1,7 +1,10 @@
 
 -- Selection without field filtering (large region ==  whole table == collection)
-SELECT 	ST_AsText(polygon)
+/* SELECT 	ST_AsText(polygon)
 FROM   	markup_polygon  
 WHERE  ST_Contains( ST_PolygonFromText('POLYGON((40960 40960, 41984 40960,  41984 41984, 40960 41984, 40960 40960))'), polygon ) = TRUE ;
+*/
 
-
+SELECT 	pais_uid,tilename,markup_id
+FROM   	markup_polygon  
+ST_Contains( ST_PolygonFromText('POLYGON((22528 8192,67584 8192,67584 24576,22528 24576,22528 8192))'), polygon ) = TRUE ;
