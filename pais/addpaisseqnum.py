eb99dfbd0542bc78419f759ba13ae15482bfecd2
Ablimit Aji <aaji@mathcs.emory.edu>
2013-03-11 17:10:45 -0400

    regenerate pais data

--- pais/addpaisseqnum.py
+++ pais/addpaisseqnum.py
@@ -7,29 +7,20 @@ import os
 
 def main():
 
-    if (len(sys.argv)<3):
-	sys.stderr.write("Missing params.\n")
-	sys.exit()
-    path   = sys.argv[1]
-    sequence = sys.argv[2]
-    
-    if not path.endswith('/'):
-	path = path + '/'
+    first = True
+    print >>sys.stdout, '|'.join(('PAIS_UID','SEQNUM','TILENAME','MARKUP_ID','POLYGON'))
+    for line in sys.stdin:
+	if first:
+	    first =False
+	    continue 
+	fields = line.strip().split("|")
+	if fields[0].endswith("1"):
+	    seqnum = '1'
+	else:
+	    seqnum = '2'
+	print >>sys.stdout, '|'.join((fields[0],seqnum,'|'.join(fields[1:])))
+    sys.stdout.flush()
 
-
-    #  print >>sys.stdout, '|'.join(('PAIS_UID','TILENAME','MARKUP_ID','POLYGON'))
-    for img in os.listdir(path):
-	f = open(path+img,'r')
-	for line in f:
-	    fields = line.strip().split(",",2)
-	    polygon = fields[-1]
-	    #fields[0] = fields[0].replace('"','')
-	    #fields[1] =fields[1].replace('"','')
-	    polygon = "POLYGON(( " + polygon[1:-1] + "))"
-	    print >>sys.stdout, '|'.join((img,fields[0],sequence,polygon))
-	sys.stdout.flush()
-	f.close()
-    
 # main function 
 main()
 
