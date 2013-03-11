662197757433a3952e64914da8149751378e2ee7
ablimit <ablimit@gmail.com>
2013-03-11 17:32:14 -0400

    Fixing join query for PAIS

--- benchmark/query/pais/gp/join/q2.sql
+++ benchmark/query/pais/gp/join/q2.sql
@@ -5,5 +5,5 @@ SELECT ST_Area(ST_Intersection(A.polygon, B.polygon)) / ST_Area(ST_Union( A.poly
     ST_DISTANCE( ST_Centroid(B.polygon), ST_Centroid(A.polygon) ) AS centroid_distance
 FROM markup_polygon A, markup_polygon B
 WHERE A.pais_uid = 'gbm1.1_40x_20x_NS-MORPH_1' AND B.pais_uid = 'gbm1.1_40x_20x_NS-MORPH_2'  AND 
-    ST_Intersects(A.polygon, B.polygon) = TRUE ;
+      A.tilename = B.tilename AND ST_Intersects(A.polygon, B.polygon) = TRUE ;
 
