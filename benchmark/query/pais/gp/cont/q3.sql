4f20058cb81fe82c59ededc37659836c89ef6bbd
ablimit <ablimit@gmail.com>
2013-03-11 14:30:06 -0400

    Containment query q3.sql revised.

--- benchmark/query/pais/gp/cont/q3.sql
+++ benchmark/query/pais/gp/cont/q3.sql
@@ -5,6 +5,6 @@ FROM   	markup_polygon
 WHERE  ST_Contains( ST_PolygonFromText('POLYGON((40960 40960, 41984 40960,  41984 41984, 40960 41984, 40960 40960))'), polygon ) = TRUE ;
 */
 
-SELECT 	pais_uid,tilename,markup_id
+SELECT 	tilename AS TID ,markup_id AS MID ,ST_AsText(polygon) AS SHAPE
 FROM   	markup_polygon  
 WHERE ST_Contains( ST_PolygonFromText('POLYGON((22528 8192,67584 8192,67584 24576,22528 24576,22528 8192))'), polygon ) = TRUE ;
