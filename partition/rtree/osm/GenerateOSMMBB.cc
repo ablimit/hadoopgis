5ab525ce32bc8ad56a13f12db1065db4cbfea87a
Ablimit Aji <aaji@mathcs.emory.edu>
2013-05-21 17:49:42 -0400

    rtree partition

---
+++ partition/rtree/osm/GenerateOSMMBB.cc
@@ -0,0 +1,54 @@
+#include <string.h>
+
+// geos 
+#include <geos/geom/PrecisionModel.h>
+#include <geos/geom/GeometryFactory.h>
+#include <geos/geom/Geometry.h>
+#include <geos/geom/Point.h>
+#include <geos/io/WKTReader.h>
+
+// boost 
+#include <boost/algorithm/string.hpp>
+
+//constants 
+#define OSM_ID 0
+#define OSM_TILEID 1
+#define OSM_OID 2
+#define OSM_ZORDER 3
+#define OSM_POLYGON 4
+
+using namespace std;
+using namespace geos;
+using namespace geos::io;
+using namespace geos::geom;
+
+/*reads in the OSM data and generates MBB for spatial objects.*/
+
+int main(int argc, char** argv)
+{
+    GeometryFactory *gf = new GeometryFactory(new PrecisionModel(),4326);
+    WKTReader *wkt_reader= new WKTReader(gf);
+    Geometry *poly ; 
+    string DEL = " ";
+    vector<string> fields;
+    double low[2], high[2];
+    string input_line ;
+    while(cin && getline(cin, input_line) && !cin.eof())
+    {
+        boost::split(fields, input_line, boost::is_any_of("|"));
+        poly = wkt_reader->read(fields[OSM_POLYGON]);
+        const Envelope * env = poly->getEnvelopeInternal();
+        low [0] = env->getMinX();
+        low [1] = env->getMinY();
+        high [0] = env->getMaxX();
+        high [1] = env->getMaxY();
+        cout << fields[OSM_ID] << DEL << low[0] << DEL << low[1] << DEL << high[0] << DEL << high[1] << endl;
+
+        fields.clear();
+    }
+    cout.flush();
+
+    return 0;
+}
+
+
---
+++ partition/rtree/pais/GeneratePAISMBB.cc
@@ -0,0 +1,48 @@
+#include <string.h>
+
+// geos 
+#include <geos/geom/PrecisionModel.h>
+#include <geos/geom/GeometryFactory.h>
+#include <geos/geom/Geometry.h>
+#include <geos/geom/Point.h>
+#include <geos/io/WKTReader.h>
+
+// boost 
+#include <boost/algorithm/string.hpp>
+
+
+using namespace std;
+using namespace geos;
+using namespace geos::io;
+using namespace geos::geom;
+
+/*reads in the PAIS data and generates MBB for spatial objects. */
+
+int main(int argc, char** argv)
+{
+    GeometryFactory *gf = new GeometryFactory(new PrecisionModel(),4326);
+    WKTReader *wkt_reader= new WKTReader(gf);
+    Geometry *poly ; 
+    string DEL = " ";
+    vector<string> fields;
+    double low[2], high[2];
+    string input_line ;
+    while(cin && getline(cin, input_line) && !cin.eof())
+    {
+        boost::split(fields, input_line, boost::is_any_of("|"));
+        poly = wkt_reader->read(fields[2]);
+        const Envelope * env = poly->getEnvelopeInternal();
+        low [0] = env->getMinX();
+        low [1] = env->getMinY();
+        high [0] = env->getMaxX();
+        high [1] = env->getMaxY();
+        cout << fields[1] << DEL << low[0] << DEL << low[1] << DEL << high[0] << DEL << high[1] << endl;
+
+        fields.clear();
+    }
+    cout.flush();
+
+    return 0;
+}
+
+
