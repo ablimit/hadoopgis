2f43a97cb262a48ddbba554a9cc7521f129a4de6
Ablimit Aji <aaji@mathcs.emory.edu>
2013-05-26 20:43:13 -0400

    trivial updates

--- partition/updateJars.sh
+++ partition/updateJars.sh
@@ -1,14 +1,14 @@
 #! /bin/bash
 
 
-loc=/home/aaji/proj/hadoopgis/xxl/xxlcore/target
+loc=../xxl/xxlcore/target
 
 for file in xxlcore-2.1-SNAPSHOT.jar xxlcore-2.1-SNAPSHOT-tests.jar
 do
     if [ -f "${loc}/${file}" ]
     then
         echo "$file found and copied."
-        cp ${loc}/${file} /home/aaji/proj/hadoopgis/partition/
+        cp ${loc}/${file} ./
     else
         echo "$file not found."
     fi
