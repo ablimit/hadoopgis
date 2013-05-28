5e1ecc36911db713ebd29c084ed9c40b8e208992
Ablimit Aji <aaji@mathcs.emory.edu>
2013-05-28 15:31:01 -0400

    bug fix in data normolization

--- partition/data/normolize.py
+++ partition/data/normolize.py
@@ -7,16 +7,15 @@ from collections import defaultdict
 
 dic = defaultdict(list)
 
-osm = False
 
 # Calculate mean of the values
-def normx(x):
+def normx(x,osm):
     if osm:
         return (x + 180.0)/360.0 
     else:
         return x/110592.0
 
-def normy(y):
+def normy(y,osm):
     if osm:
         return (y + 90.0)/180.0
     else:
@@ -27,18 +26,19 @@ def main():
         print "Usage: "+ sys.argv[0] + " [osm| pais]"
         sys.exit(1)
 
+    osm = False
     if "osm" in sys.argv[1].lower():
         osm = True
-    else:
-        osm = False
+
 
     for line in sys.stdin:
         sp = line.strip().split()
         if (len(sp)>4):
-            x1 = normx(float(sp[1]))
-            y1 = normy(float(sp[2]))
-            x2 = normx(float(sp[3]))
-            y2 = normy(float(sp[4]))
+            x1 = normx(float(sp[1]),osm)
+            y1 = normy(float(sp[2]),osm)
+            x2 = normx(float(sp[3]),osm)
+            y2 = normy(float(sp[4]),osm)
+	    # print "after norm: %f" % x1
             print "\t".join((sp[0],str(x1),str(y1),str(x2),str(y2)))
         #else:
         #print len(sp)
