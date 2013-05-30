e882ff4c8f3f688e7d67158560fd8d41260c1311
Ablimit Aji <aaji@mathcs.emory.edu>
2013-05-30 16:25:59 -0400

    trivial

--- partition/doosm.sh
+++ partition/doosm.sh
@@ -6,17 +6,11 @@ echo "$f"
 lc=`wc -l /data2/ablimit/Data/spatialdata/osmout/${f}.dat | cut -d' ' -f1 `
 p=`expr $((lc/5000000))`
 
+gridsize=11
+alpha=0.1
+ratio=0.4
+sample=0.7
 
-java -Xss4m -Xmx80000M -cp xxlcore-2.1-SNAPSHOT.jar:xxlcore-2.1-SNAPSHOT-tests.jar:. xxl.core.spatial.TestH1 $p /data2/ablimit/Data/spatialdata/osmout/${f}.dat data/partres/${f} 
-$1 $2 $3 $4
 
-#rtree.sh
-#minskew.sh
-./minskew.sh $p /data2/ablimit/Data/spatialdata/osmout/${f}.dat data/partres/${f}.minskew.txt I 10
-#rkHist.sh
-./rkHist.sh $p /data2/ablimit/Data/spatialdata/osmout/${f}.dat data/partres/${f}.rkHist.txt 0.1
-#rv.sh
-./rv.sh $p /data2/ablimit/Data/spatialdata/osmout/${f}.dat data/partres/${f}.rv.txt 0.4 
-#stHist.sh
-./stHist.sh $p /data2/ablimit/Data/spatialdata/osmout/${f}.dat data/partres/${f}.sthist.txt 0.5
+java -Xss4m -Xmx80000M -cp xxlcore-2.1-SNAPSHOT.jar:xxlcore-2.1-SNAPSHOT-tests.jar:. xxl.core.spatial.TestH1 $p /data2/ablimit/Data/spatialdata/osmout/${f}.dat data/partres/${f} ${gridsize} ${alpha} ${ratio} ${sample}
 
