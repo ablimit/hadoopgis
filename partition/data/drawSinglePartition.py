9b775e82a1f4121ddda6879875d03913727afb44
Ablimit Aji <aaji@mathcs.emory.edu>
2013-06-02 15:07:25 -0400

    visulization tools

---
+++ partition/data/drawSinglePartition.py
@@ -0,0 +1,35 @@
+#! /home/aaji/softs/bin/python
+
+import sys
+import math
+
+def main():
+    if len(sys.argv) <2:
+        print "Usage: "+ sys.argv[0] + " [output image path]"
+        sys.exit(1)
+    path = sys.argv[1].strip()
+
+    print "set terminal pngcairo size 1024,768 enhanced font 'Verdana,20'"
+    print "set output '{0}'".format(path)
+    print "unset xtics" 
+    print "unset ytics"
+
+    oid = 0
+    for line in sys.stdin:
+        sp = line.strip().split()
+        if (len(sp)>4):
+            oid += 1
+            x1 = sp[1]
+            y1 = sp[2]
+            x2 = sp[3]
+            y2 = sp[4]
+            print "set object {0} rect from {1}, {2} to {3}, {4}".format(oid,x1,y1,x2,y2)
+    
+    print "plot [-0.05:1.05] [-0.05:1.05] NaN notitle "
+
+    sys.stdout.flush()
+    sys.stderr.flush()
+
+if __name__ == '__main__':
+    main()
+
--- partition/data/drawpartition.sh
+++ partition/data/drawpartition.sh
@@ -2,29 +2,19 @@
 
 tempf=/tmp/zahide.gnu
 
-for f in astroII.1 astroII.2 gbm0.1 gbm0.2 gbm1.1 gbm1.2 gbm2.1 gbm2.2 normal.2 normal.3 oligoastroII.1 oligoastroII.2 oligoastroIII.1 oligoastroIII.2 oligoII.1 oligoII.2 oligoIII.1 oligoIII.2
+for f in astroII.1 
+    # astroII.2 gbm0.1 gbm0.2 gbm1.1 gbm1.2 gbm2.1 gbm2.2 normal.2 normal.3 oligoastroII.1 oligoastroII.2 oligoastroIII.1 oligoastroIII.2 oligoII.1 oligoII.2 oligoIII.1 oligoIII.2
 do
     echo "drawing ${f} .."
     for method in rtree minskew rv rkHist sthist 
     do
         if [ -e partres/${f}.${method}.txt ]
         then
-            echo "set terminal pngcairo size 1024,768 enhanced font 'Verdana,20'" > ${tempf}
-            echo "set output 'pics/${f}.${method}.png'" >> ${tempf}
-            echo "unset xtics" >> ${tempf}
-            echo "unset ytics" >> ${tempf}
+            python drawSinglePartition.py pics/${f}.${method}.png < partres/${f}.${method}.txt >> ${tempf}
+            gnuplot ${tempf}
+        fi
 
-            cat partres/${f}.${method}.txt | while read oid x y xx yy a;
-        do
-            oid=`expr $oid + 1`
-            echo "set object ${oid} rect from ${x}, ${y} to ${xx}, ${yy}" >> ${tempf}
-        done
-        echo "plot [-0.05:1.05] [-0.05:1.05] NaN notitle "  >> ${tempf}
-        gnuplot ${tempf}
-    fi
-
-done
-
+    done
 
 done
 
