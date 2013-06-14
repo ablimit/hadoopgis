91974b24cf694aeaa64f08385dacc7c65b79d2e7
Ablimit Aji <aaji@mathcs.emory.edu>
2013-06-14 00:09:07 -0400

    map function

--- partition/benchmark/join/pais/mapper.cpp
+++ partition/benchmark/join/pais/mapper.cpp
@@ -6,8 +6,7 @@
 #include <boost/algorithm/string.hpp>
 
 using namespace std;
-using namespace std;
-using boost::lexical_cast;
+//using boost::lexical_cast;
 
 int GEOM_IDX = 4;
 int ID_IDX = 3;
@@ -15,18 +14,19 @@ int TILE_IDX = 2;
 int SET_IDX = 1;
 int IMAGE_IDX = 0;
 
+string DEL = "|";
+string TAB = "\t";
+string DASH = "-";
+
 
 vector<string> parse(string & line) {
     vector<string> vec ;
-    boost::split(vec, line, boost::is_any_of(BAR));
+    boost::split(vec, line, boost::is_any_of(DEL));
     return vec;
 }
 
 int main(int argc, char **argv) {
 
-    string DEL = "|";
-    string TAB = "\t";
-    string DASH = "-";
     size_t pos ;
     string image_name;
     string input_line;
@@ -36,7 +36,7 @@ int main(int argc, char **argv) {
 	fields = parse(input_line);
 	pos = fields[IMAGE_IDX].find_first_of(DASH);
 	image_name = fields[IMAGE_IDX].substr(0,pos);
-	cout << image_name << fields[TILE_IDX] << TAB << fields[SET_IDX] << BAR << fields[ID_IDX] <<BAR << fields[GEOM_IDX] <<endl;
+	cout << image_name << DASH<<fields[TILE_IDX] << TAB << fields[SET_IDX] << DEL << fields[ID_IDX] <<DEL << fields[GEOM_IDX] <<endl;
 	fields.clear();
     }
     cout.flush();
