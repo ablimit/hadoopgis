ccb1d7f38ef12bae054bf96571555948f665da67
Ablimit Aji <ablimit@gmail.com>
2013-11-18 23:24:32 -0500

    rplustree visualization test

---
+++ partition/data/vispart/rplustree/vispartition.sh
@@ -0,0 +1,13 @@
+#! /bin/bash
+
+if [ ! -d pics ];
+then 
+    mkdir -p pics
+fi
+
+for tid in 2 3 4 5
+do
+    python drawSinglePartition.py ${PWD}/pics/test.${tid}.png meta/regionmbb.${tid}.txt ../testdata/test${tid}.obj.txt > meta/plot.${tid}.gpl
+    gnuplot meta/plot.${tid}.gpl
+done
+
