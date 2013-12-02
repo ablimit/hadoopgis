e064efa4fd78bbe11db542afe724798f4b8baaba
Ablimit Aji <aaji@mathcs.emory.edu>
2013-12-02 11:15:10 -0500

    minor change

--- partition/evalallpartitions.sh
+++ partition/evalallpartitions.sh
@@ -6,7 +6,7 @@ data=/scratch/data/osm.mbb.norm.filter.dat
 opath=/scratch/data/partition/osm
 for k in 864 4322 8644 17288 43220 86441 172882 432206 864412 4322062
 do
-  for algo in rp rt
+  for algo in rp rt fg
   do
     echo "[${k}] [${algo}]"
     echo -n "${algo},${k}," >> osm.eval.csv
@@ -28,7 +28,7 @@ opath=/scratch/data/partition/pais
 
 for k in 20 100 200 400 1000 2000 4000 10000 20000 10000
 do
-  for algo in rp rt
+  for algo in rp rt fg
   do
     echo "[${k}] [${algo}]"
     echo -n "${algo},${k}," >> pais.eval.csv
