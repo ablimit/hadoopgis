94966bf213a75ca4a42f6e584857b067837f4634
Ablimit Aji <ablimit@gmail.com>
2013-12-25 18:39:45 -0500

    tcga script

---
+++ tcga/getCluster.sh
@@ -0,0 +1,10 @@
+#! /bin/bash
+
+# Tiling first data file
+# --ami-version 3.0.2 
+# --num-instances=18 --instance-type=c1.xlarge --master-instance-type=m1.medium 
+elastic-mapreduce --create --stream --enable-debugging --instance-group master --instance-type m1.medium --instance-count 1 --instance-group core --instance-type m1.large --instance-count 23 --instance-group task --instance-type m1.large --instance-count 25 --name 'TCGA'  --bootstrap-action 's3://aaji/scratch/awsjoin/bootcopy.sh' --bootstrap-action s3://elasticmapreduce/bootstrap-actions/configure-hadoop --bootstrap-name "Set Maximum Cuncurrent Reduce Task" --args "-m,mapred.tasktracker.reduce.tasks.maximum=3" --bootstrap-action s3://elasticmapreduce/bootstrap-actions/configure-hadoop --bootstrap-name "Disable reducer speculative execution" --args "-m,mapred.reduce.tasks.speculative.execution=false" --region us-east-1 --log-uri 's3://aaji/scratch/logs' --key-pair aaji --mapper 's3://aaji/scratch/awsjoin/tcgamapper.py' --reducer "s3://aaji/scratch/deps/bins/resque st_intersects 1 1" --input s3://aaji/data/tcga --output s3://aaji/scratch/pout/dec25/tcga_1 --jobconf mapred.reduce.tasks=2000 
+
+# --step-action TERMINATE_JOB_FLOW
+# --step-name "tcga.st_intersect.full" 
+
