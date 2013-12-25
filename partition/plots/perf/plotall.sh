fce0c1e7162925a606ecad84a7c922847b2bcc4c
Ablimit Aji <ablimit@gmail.com>
2013-12-25 14:44:07 -0500

    plots for join query performance test

---
+++ partition/plots/perf/plotall.sh
@@ -0,0 +1,54 @@
+#! /bin/bash
+
+usage(){
+    echo -e "plotall.sh  [ pais | osm ]\n \
+    --name \t dataset name to process \n \
+    --help \t show this information.
+    "
+    exit 1
+}
+
+name=""
+
+while :
+do
+    case $1 in
+	-h | --help | -\?)
+	    usage;
+	    #  Call your Help() or usage() function here.
+	    exit 0      # This is not an error, User asked help. Don't do "exit 1"
+	    ;;
+	-n | --name)
+	    name=$2     # You might want to check if you really got FILE
+	    shift 2
+	    ;;
+	--name=*)
+	    name=${1#*=}        # Delete everything up till "="
+	    shift
+	    ;;
+	--) # End of all options
+	    shift
+	    break
+	    ;;
+	-*)
+	    echo "WARN: Unknown option (ignored): $1" >&2
+	    shift
+	    ;;
+	*)  # no more options. Stop while loop
+	    break
+	    ;;
+    esac
+done
+
+
+if [ ! "$name" ] ; then
+  echo "ERROR: missing option. See --help" >&2
+  exit 1
+fi
+
+# python genPlotData.py "${metric}" < ${name}.eval.csv > pltdata.dat
+cp template.plt draw.plt
+perl -p -i -e "s/_chartname_/${name}.medium.eps/g" draw.plt
+perl -p -i -e "s/_dataset_/${name}.medium.100.dat/g" draw.plt
+perl -p -i -e "s/_keyposition_/left top/g" draw.plt
+gnuplot draw.plt
