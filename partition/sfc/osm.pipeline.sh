ffff6ee0f110336fc2746186f2b8866278a03885
Ablimit Aji <aaji@mathcs.emory.edu>
2014-01-06 17:25:55 -0500

    sfc

--- partition/sfc/osm.pipeline.sh
+++ partition/sfc/osm.pipeline.sh
@@ -3,64 +3,76 @@
 indexCapacity=1000
 fillFactor=0.99
 
-# 864 4322 8644 17288 43220 86441 172882 432206 864412 4322062
+anchor=center
+tempPath=/dev/shm/osm/hc
 
-ipath=/scratch/data/osm.mbb.norm.filter.dat
-opath=/scratch/data/partition/osm/rp
-tempPath=/dev/shm/osm/rp
+mkdir -p ${tempPath}
+
+echo -e "\n------------------------------------"
+echo "building rtree index on test ...."
+../genRtreeIndex /data2/ablimit/Data/spatialdata/bakup/data/osm.mbb.norm.filter.dat ${tempPath}/spatial 20 1000 $fillFactor
+rc=$?
+if [ $rc -eq 0 ];then
+  echo ""
+else
+  echo -e "\nERROR: genRtreeIndex failed."
+  exit $rc ;
+fi
 
 
-for k in 864 4322 8644 17288 43220 86441 172882 432206 864412 4322062
-do
-  if [ ! -e ${opath}/c${k} ] ;
-  then
-    mkdir -p ${opath}/c${k}
-  fi
+for anchor in min center max
+do 
+  rawdatapath=/data2/ablimit/Data/spatialdata/bakup/data/partition/osm/hc/osm.mbb.hc.sorted.${anchor}.dat
+  opath=/data2/ablimit/Data/spatialdata/bakup/data/partition/osm/hc/${anchor}
+  cut -d" " -f1,2,3,4,5 ${rawdatapath}  > ${tempPath}/hilbert.dat
+  ipath=${tempPath}/hilbert.dat
+
+
+  for k in 864 4322 8644 17288 43220 86441 172882 432206 864412 4322062
+  do
+    if [ ! -e ${opath}/c${k} ] ;
+    then
+      mkdir -p ${opath}/c${k}
+    fi
+
+    echo "partition size ${k} K"
+
+
+    ../hilbertPartition ${tempPath}/hilbert.dat ${k} > ${opath}/c${k}/regionmbb.txt
+    rc=$?
+
+    if [ $rc -eq 0 ];then
+      echo ""
+    else
+      echo -e "\nERROR: partition generation failed "
+      exit $rc ;
+    fi
+
+
+    echo -e "---------------------------------------------"
+    echo "generate pid oid mapping ...."
+    ../rquery ${opath}/c${k}/regionmbb.txt ${tempPath}/spatial  > ${tempPath}/pidoid.txt
+    rc=$?
+    if [ $rc -eq 0 ];then
+      echo ""
+    else
+      echo -e "\nERROR: rqueryfailed."
+      exit $rc ;
+    fi
+
+    echo -e "\n---------------------------------------------"
+    echo "remapping objects"
+    python ../mappartition.py ${tempPath}/pidoid.txt < /data2/ablimit/Data/spatialdata/bakup/data/osm.mbb.norm.filter.dat > ${opath}/c${k}/osm.part
+
+    rm -f ${tempPath}/pidoid.txt
   
-  echo "partition size ${k} K"
+  done
   
-  cp c${k}.txt ${opath}/c${k}/regionmbb.txt
-  
-  python ../simulatecerr.py < ${opath}/c${k}/regionmbb.txt > ${opath}/c${k}/idxmbb.gnu
-  
-  rc=$?
-  
-  if [ $rc -eq 0 ];then
-    echo ""
-  else
-    echo -e "\nERROR: gnuplot generation failed "
-    exit $rc ;
-  fi
+  rm -f ${tempPath}/hilbert.dat
 
-  echo -e "\n------------------------------------"
-  echo "building rtree index on test ...."
-  ../genRtreeIndex ${ipath} ${tempPath}/spatial 20 1000 $fillFactor
-  rc=$?
-  if [ $rc -eq 0 ];then
-    echo ""
-  else
-    echo -e "\nERROR: genRtreeIndex failed."
-    exit $rc ;
-  fi
-
-  echo -e "---------------------------------------------"
-  echo "generate pid oid mapping ...."
-  ../rquery ${opath}/c${k}/regionmbb.txt ${tempPath}/spatial  > ${tempPath}/pidoid.txt
-  rc=$?
-  if [ $rc -eq 0 ];then
-    echo ""
-  else
-    echo -e "\nERROR: rqueryfailed."
-    exit $rc ;
-  fi
-
-  echo -e "\n---------------------------------------------"
-  echo "remapping objects"
-  python ../mappartition.py ${tempPath}/pidoid.txt < ${ipath} > ${opath}/c${k}/osm.part
-
-  rm ${tempPath}/spatial*
-  rm ${tempPath}/pidoid.txt 
 done
+
+rm -f ${tempPath}/spatial*
 
 touch done.osm.log
 
