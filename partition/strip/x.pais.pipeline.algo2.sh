f4272db79942dbe810ee7c7793665f7564c23d73
Ablimit Aji <aaji@mathcs.emory.edu>
2014-01-20 22:27:13 -0500

    pais secondary dataset generator

---
+++ partition/strip/x.pais.pipeline.algo2.sh
@@ -0,0 +1,58 @@
+#! /bin/bash
+
+indexCapacity=1000
+fillFactor=0.99
+
+ipath=/data2/ablimit/Data/spatialdata/pais/mbb
+geompath=/data2/ablimit/Data/spatialdata/pais/geom
+opath=/scratch/data/partition/pais/st/x
+tempPath=/dev/shm/osm/st/x
+
+mkdir -p ${tempPath}
+
+for image in astroII.1 astroII.2 gbm0.1 gbm0.2 gbm1.1 gbm1.2 gbm2.1 gbm2.2 normal.2 normal.3 oligoastroII.1 oligoastroII.2 oligoastroIII.1 oligoastroIII.2 oligoII.1 oligoII.2 oligoIII.1 oligoIII.2
+do
+
+  for k in 20 100 200 400 1000 2000 4000 10000 20000 100000
+  do
+    if [ ! -e ${opath}/c${k} ] ;
+    then
+      mkdir -p ${opath}/c${k}
+    fi
+
+    echo -e "\n------------------------------------"
+    echo "building rtree index on test ...."
+    ../genRtreeIndex ${ipath}/${image}.norm.2.dat ${tempPath}/spatial 20 1000 $fillFactor
+
+    rc=$?
+    if [ $rc -eq 0 ];then
+      echo ""
+    else
+      echo -e "\nERROR: genRtreeIndex failed."
+      exit $rc ;
+    fi
+
+    echo -e "---------------------------------------------"
+    echo "generate pid oid mapping ...."
+    ../rquery ${opath}/c${k}/${image}.regionmbb.txt ${tempPath}/spatial  > ${tempPath}/pidoid.txt
+    rc=$?
+    if [ $rc -eq 0 ];then
+      echo ""
+    else
+      echo -e "\nERROR: rquery failed."
+      exit $rc ;
+    fi
+
+    echo -e "\n---------------------------------------------"
+    echo "remapping objects"
+    python ../mappartition.py ${tempPath}/pidoid.txt < ${ipath}/${image}.norm.2.dat > ${opath}/c${k}/${image}.2.part
+
+    rm ${tempPath}/spatial*
+    rm ${tempPath}/pidoid.txt 
+    
+    python ../remappaistogeom.py ${opath}/${algo}/c${k}/${image}.2.part < ${geompath}/${image}.2.dat > ${opath}/${algo}/c${k}/${image}.geom.2.dat
+  
+  done
+done
+
+touch "done.x.pais.log"
