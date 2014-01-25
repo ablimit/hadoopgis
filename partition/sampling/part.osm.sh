599353390750a3cb3d09e3526dd8dd9ae7ec28be
Ablimit Aji <aaji@gpgpu0.(none)>
2014-01-25 15:53:26 -0500

    removal

--- partition/sampling/part.osm.sh
+++
@@ -1,8 +0,0 @@
-#! /bin/bash
-
-for f in 01 05 10 15 20 25 
-do
-  echo "sampling ${f}%"
-  cat  /home/aaji/proj/data/osm/osm.mbb.norm.filter.dat | awk "BEGIN {srand()} !/^$/ { if (rand() <= .${f}) print \$0}" > /home/aaji/proj/data/sampling/osm/osm.sample.${f}.dat
-done
-
