efc4a62c2872dacd5f350cb4082e6ada1f26c59d
Ablimit Aji <ablimit@gmail.com>
2014-01-29 23:11:25 -0500

    hc precision experiments

---
+++ partition/sfc/precexp/osm.pipeline.sh
@@ -0,0 +1,25 @@
+#! /bin/bash
+
+ipath=/home/aaji/proj/data/osm/osm.mbb.norm.filter.dat
+prog=./hc
+
+for p in 4 8 10 12 14 16 18 20 25 30
+do
+  echo "--------------------------------"
+  echo ${dest}
+
+  ${prog}  --prec ${p} --bucket ${param} --input ${ipath} # > ${dest}/regionmbb.txt
+  rc=$?
+  if [ ! $rc -eq 0 ];then
+    echo -e "\nERROR: partition generation failed."
+    exit $rc ;
+  fi
+
+  for k in 864 4322 8644 17288 43220 86441 172882 432206 864412 4322062
+  do
+    dest=/home/aaji/proj/data/prec/p${p}/c${k}
+    mkdir -p ${dest}
+    mv c${k}.txt ${dest}/regionmbb.txt
+  done
+done
+
