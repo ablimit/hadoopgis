e96efd04d1e83eefdf528984e10c73501fbf3182
Ablimit Aji <aaji@mathcs.emory.edu>
2014-02-05 00:42:22 -0500

    statistical sampling partition evaluation

--- partition/evalsamp.sh
+++ partition/evalsamp.sh
@@ -1,15 +1,20 @@
 #! /bin/bash
 
-# osm
-data=/scratch/data/osm.mbb.norm.filter.dat
+hid=$(hostname)
+opath=/home/aaji/proj/hadoopgis/partition/sampling/stateval
+data=/scratch/ablimit/data/partition/samp/osm
 
-opath=/scratch/data/partition/osm
-for k in 864 4322 8644 17288 43220 86441 172882 432206 864412 4322062
+for algo in fg bsp hc slc bos str
 do
-  for algo in rp rt fg
+  for f in 01 05 10 15 20 25 
   do
-    echo "[${k}] [${algo}]"
-    echo -n "${algo},${k}," >> osm.eval.csv
-    python evalpartition.py 86441255 < ${opath}/${algo}/c${k}/osm.part >>osm.eval.csv
+    for k in 864 4322 8644 17288 43220 86441 172882 432206 864412 4322062
+    do
+      if [ -e ${data}/${algo}/f${f}/c${k}/osm.part ]; then
+      echo -n "${algo},${f},${k}," >> ${opath}/osm.eval.csv.${hid}
+      python evalpartition.py 86441255 < ${data}/${algo}/f${f}/c${k}/osm.part >> ${opath}/osm.eval.csv.${hid}
+    fi
+    done
   done
 done
+
