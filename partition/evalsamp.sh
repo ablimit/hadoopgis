981f918a41e07c3fd223fe2773e6b124ca553c55
Ablimit Aji <aaji@mathcs.emory.edu>
2014-02-11 20:08:38 -0500

    update

--- partition/evalsamp.sh
+++ partition/evalsamp.sh
@@ -6,15 +6,21 @@ data=/scratch/ablimit/data/partition/samp/osm
 
 for algo in fg bsp hc slc bos str
 do
-  for f in 01 05 10 15 20 25 
-  do
-    for k in 864 4322 8644 17288 43220 86441 172882 432206 864412 4322062
+  if [ ! -e sampling/parr/eval.${algo} ] ;
+  then
+    touch sampling/parr/eval.${algo} ;
+    for f in 01 05 10 15 20 25 
     do
-      if [ -e ${data}/${algo}/f${f}/c${k}/osm.part ]; then
-      echo -n "${algo},${f},${k}," >> ${opath}/osm.eval.csv.${hid}
-      python evalpartition.py 86441255 < ${data}/${algo}/f${f}/c${k}/osm.part >> ${opath}/osm.eval.csv.${hid}
-    fi
+      for k in 864 4322 8644 17288 43220 86441 172882 432206 864412 4322062
+      do
+        if [ -e ${data}/${algo}/f${f}/c${k}/osm.part ]; then
+          echo "${algo},${f},${k}"
+          echo -n "${algo},${f},${k}," >> ${opath}/osm.eval.csv.${hid}
+          python evalpartition.py 86441255 < ${data}/${algo}/f${f}/c${k}/osm.part >> ${opath}/osm.eval.csv.${hid}
+          echo "------------------------------------"
+        fi
+      done
     done
-  done
+  fi
 done
 
--- partition/sfc/precexp/osm.pipeline.sh
+++ partition/sfc/precexp/osm.pipeline.sh
@@ -3,36 +3,49 @@
 ipath=/data2/ablimit/Data/spatialdata/bakup/data/osm.mbb.norm.filter.dat
 opath=/data2/ablimit/Data/spatialdata/bakup/data/prec
 
-tempPath=/dev/shm/osm/prec
+TEMPPATH=`mktemp -d -p /dev/shm`
 indexPath=/dev/shm
 mkdir -p ${tempPath}
 
-for p in 4 8 10 12 14 16 18 20 25 30
+for p in 10 12 14 16 18 20 25 30
 do
-  for k in 864 4322 8644 17288 43220 86441 172882 432206 864412 4322062
-  do
-    if [ ! -e ${opath}/p${p}/c${k}/regionmbb.txt ] ;
-    then
-      continue;
-    fi
+  if [ ! -e parr/occu.${p} ] ;
+  then
+    touch parr/occu.${p} ;
 
-    echo "--------- ${k} --------------"
-    echo "generate pid oid mapping ...."
-    ../../rquery ${opath}/p${p}/c${k}/regionmbb.txt ${indexPath}/spatial  > ${tempPath}/pidoid.txt
-    rc=$?
-    if [ $rc -eq 0 ];then
-      echo ""
-    else
-      echo -e "\nERROR: rqueryfailed."
-      exit $rc ;
-    fi
+    for k in 864 4322 8644 17288 43220 86441 172882 432206 864412 4322062
+    do
+      if [ ! -e ${opath}/p${p}/c${k}/regionmbb.txt ] ;
+      then
+        continue;
+      fi
 
-    echo -e "\n---------------------------------------------"
-    echo "remapping objects"
-    python ../../mappartition.py ${tempPath}/pidoid.txt < ${ipath} > ${opath}/p${p}/c${k}/osm.part
+      echo "--------- ${k} --------------"
+      echo "generate pid oid mapping ...."
+      ../../rquery ${opath}/p${p}/c${k}/regionmbb.txt ${indexPath}/spatial  > ${TEMPPATH}/pidoid.txt
+      rc=$?
+      if [ $rc -eq 0 ];then
+        echo ""
+      else
+        echo -e "\nERROR: rqueryfailed."
+        exit $rc ;
+      fi
 
-    rm -f ${tempPath}/pidoid.txt
+      echo -e "\n---------------------------------------------"
+      echo "remapping objects"
+      python ../../mappartition.py ${TEMPPATH}/pidoid.txt < ${ipath} > ${opath}/p${p}/c${k}/osm.part
+      rc=$?
+      if [ $rc -eq 0 ];then
+        echo ""
+      else
+        echo -e "\nERROR: partition mapping failed."
+        exit $rc ;
+      fi
 
-  done
+      rm -f ${TEMPPATH}/pidoid.txt
+    done
+  fi
 done
+
+rm -rf ${TEMPPATH}
 
