497f1a39a1b68c41db12a2d5925386f2b54f0b4e
Ablimit Aji <aaji@mathcs.emory.edu>
2014-02-19 06:42:14 -0500

    pais bsp

--- partition/aws/upload/upload.pais.hc.sh
+++ partition/aws/upload/upload.pais.hc.sh
@@ -1,8 +1,9 @@
 #! /bin/bash
 
 # hilbert curve partition 
-algo=hc
-aws s3 sync /data2/ablimit/Data/spatialdata/bakup/data/partition/pais/${algo} s3://aaji/data/partitions/pais/${algo} --exclude '*' --include '*pais.geom*'
+algo=bsp
+# aws s3 --dryrun sync /data/ablimit/Data/spatialdata/bakup/data/partition/pais/${algo} s3://aaji/data/partitions/pais/${algo} --exclude '*' --include '*pais.geom*'
+aws s3 sync /data/ablimit/Data/spatialdata/bakup/data/partition/pais/${algo} s3://aaji/data/partitions/pais/${algo} --exclude '*' --include '*pais.geom*'
 
 
 
--- partition/remappais.sh
+++ partition/remappais.sh
@@ -1,30 +1,86 @@
 #! /bin/bash
 
+usage(){
+  echo -e "remappais.sh  options \n \
+    --alg \t [ fg | bsp | hc | st | rp | rt ] \n \
+    --help \t show this information.
+  "
+  exit 1
+}
+
+algo=""
+
+while :
+do
+  case $1 in
+    -h | --help | -\?)
+      usage;
+      #  Call your Help() or usage() function here.
+      exit 0      # This is not an error, User asked help. Don't do "exit 1"
+      ;;
+    -a | --alg)
+      algo=$2     # You might want to check if you really got FILE
+      shift 2
+      ;;
+    --alg=*)
+      algo=${1#*=}        # Delete everything up till "="
+      shift
+      ;;
+    --) # End of all options
+      shift
+      break
+      ;;
+    -*)
+      echo "WARN: Unknown option (ignored): $1" >&2
+      shift
+      ;;
+    *)  # no more options. Stop while loop
+      break
+      ;;
+  esac
+done
+
+
+# argument checking 
+if [ "${algo}" != "st" ] && [ "${algo}" != "rt" ] && [ "${algo}" != "rp" ] && [ "${algo}" != "fg" ] && [ "${algo}" != "hc" ] && [ "${algo}" != "bsp" ];
+then
+  echo "Parameter [${algo}] is NOT recognized. Alternatives are [ st | rp | rt | fg | hc | bsp ]"
+  exit 1;
+fi
 # osm
 geompath=/data2/ablimit/Data/spatialdata/pais/geom
+opath=/data/ablimit/Data/spatialdata/bakup/data/partition/pais
 
-opath=/scratch/data/partition/pais
+if  [ ! -e ${opath}/${algo} ] ; then 
+  echo "no data is present !"
+  exit 1; 
+fi
 
-logg=pais.remap.log
-
-> ${logg}
 
 for image in astroII.1 astroII.2 gbm0.1 gbm0.2 gbm1.1 gbm1.2 gbm2.1 gbm2.2 normal.2 normal.3 oligoastroII.1 oligoastroII.2 oligoastroIII.1 oligoastroIII.2 oligoII.1 oligoII.2 oligoIII.1 oligoIII.2
 do
   for k in 20 100 200 400 1000 2000 4000 10000 20000 100000
   do
 
-    for algo in rp rt fg
-    do
-      echo "[${k}] [${algo}]"
-      echo "[${k}] [${algo}]" >> ${logg}
+    if [ "${algo}" == "rt" ] || [ "${algo}" == "rp" ] || [ "${algo}" == "fg" ] || [ "${algo}" == "bsp" ]; then
+      
+      echo "set 1 ---- ${image} --- ${k} --- "
       python remappaistogeom.py ${opath}/${algo}/c${k}/${image}.part < ${geompath}/${image}.1.dat > ${opath}/${algo}/c${k}/${image}.geom.1.dat
-    done
-
-    algo=st
-    echo "[${k}] [${algo}]"
-    echo "[${k}] [${algo}]"   >> ${logg}
-    python remappaistogeom.py ${opath}/${algo}/x/c${k}/${image}.part < ${geompath}/${image}.1.dat > ${opath}/${algo}/x/c${k}/${image}.geom.1.dat
+      
+      echo "set 2 ---- ${image} --- ${k} --- "
+      python remappaistogeom.py ${opath}/${algo}/c${k}/${image}.2.part < ${geompath}/${image}.2.dat > ${opath}/${algo}/c${k}/${image}.geom.2.dat
+    
+    elif [ "${algo}" == "st" ]; then 
+      
+      echo "[${k}] [${algo}]"
+      python remappaistogeom.py ${opath}/${algo}/x/c${k}/${image}.part < ${geompath}/${image}.1.dat > ${opath}/${algo}/x/c${k}/${image}.geom.1.dat
+      
+      python remappaistogeom.py ${opath}/${algo}/x/c${k}/${image}.2.part < ${geompath}/${image}.2.dat > ${opath}/${algo}/x/c${k}/${image}.geom.2.dat
+    
+    else 
+      echo "Dumb input !"
+      exit 1;
+    fi
   done
 done
 
