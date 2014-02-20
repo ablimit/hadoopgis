ce27db28c5ea67b698f2289af88c0a4c75d9d484
Ablimit Aji <aaji@mathcs.emory.edu>
2014-02-20 17:04:39 -0500

    partitioner completed.

--- partition/mapred/partitioner/src/main/java/corner/Point.java
+++ partition/mapred/partitioner/src/main/java/corner/Point.java
@@ -1,28 +1,34 @@
-package abby;
+package corner;
 
-public class Point implements Comparable<Point>{
-    private double x;
-    private double y;
-    public Point(double x, double y) {
-      super();
-      this.x = x;
-      this.y = y;
-    }
+public class Point implements Comparable<Point> {
+	private double x;
+	private double y;
 
-    public int compareTo(Point p) {
-      if (this.x < p.x ) 
-        return 1; 
-      else if (this.x > p.x)
-        return -1; 
-      else 
-      {
-      if (this.y < p.y ) 
-        return 1; 
-      else if (this.y > p.y)
-        return -1; 
-      else 
-        return 0;
-      }
-    }
-  }
+	public Point(double x, double y) {
+		super();
+		this.x = x;
+		this.y = y;
+	}
 
+	public int compareTo(Point p) {
+		if (this.x < p.x) {
+			//System.err.println("x:"+this.x + "<" + p.x);
+			return -1;
+		} else if (this.x > p.x) {
+			//System.err.println("x:"+this.x + ">" + p.x);
+			return 1;
+		} else {
+			if (this.y < p.y) {
+				//System.err.println("y:"+this.y + "<" + p.y);
+				return -1;
+			} else if (this.y > p.y) {
+				//System.err.println("y:"+this.y + ">" + p.y);
+				return 1;
+			} else {
+				//System.err.println(this.x + "," + this.y + "==" + p.x + ","
+						//+ p.y);
+			}
+			return 0;
+		}
+	}
+}
