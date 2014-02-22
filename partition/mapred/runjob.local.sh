e92c312f16d531eaa6a97b8d7ab528a878c45a78
ablimit aji <ablimit@gmail.com>
2014-02-22 17:32:35 -0500

    working copy of the sorting script

--- partition/mapred/runjob.local.sh
+++ partition/mapred/runjob.local.sh
@@ -1,12 +1,19 @@
 #! /bin/env bash
 
-$HADOOP_HOME/bin/hadoop  jar $HADOOP_HOME/hadoop-streaming.jar \
+hdfs dfs -rm -R /user/aaji/temp/sort ;
+
+$HADOOP_COMMON_HOME/bin/hadoop jar $HADOOP_COMMON_HOME/share/hadoop/tools/lib/hadoop-streaming-2.2.0.jar \
   -D mapred.output.key.comparator.class=org.apache.hadoop.mapred.lib.KeyFieldBasedComparator \
-  -D map.output.key.field.separator=" " \
-  -D mapred.text.key.comparator.options=-kn1,2n \
+  -D map.output.key.field.separator=, \
+  -D mapred.text.key.comparator.options=-k1,2n \
+  -libjars corner-0.0.1.jar \
+  -files splitpoints.dat,centerMapper.py \
   -D mapred.reduce.tasks=100 \
-  -input myInputDirs \
-  -output myOutputDir \
-  -mapper org.apache.hadoop.mapred.lib.IdentityMapper \
-  -reducer org.apache.hadoop.mapred.lib.IdentityReducer
+  -input /user/aaji/osm/osm.mbb.norm.filter.dat\
+  -output /user/aaji/temp/sort \
+  -mapper centerMapper.py \
+  -reducer org.apache.hadoop.mapred.lib.IdentityReducer \
+  -partitioner CornerPartitioner
+
+#  -mapper org.apache.hadoop.mapred.lib.IdentityMapper \
 
