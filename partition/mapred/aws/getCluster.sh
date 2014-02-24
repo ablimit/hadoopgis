3cc4d3c0c8a8323a735c415b2eab76adf1be732a
Ablimit Aji <aaji@mathcs.emory.edu>
2014-02-24 03:59:11 -0500

    tri

--- partition/mapred/aws/getCluster.sh
+++ partition/mapred/aws/getCluster.sh
@@ -1,18 +1,20 @@
 #! /bin/bash
 
 # Tiling first data file
-# --ami-version 3.0.2 
 # --num-instances=18 --instance-type=c1.xlarge --master-instance-type=m1.medium 
+# --ami-version 3.0.4
 
 maxred=2
 maxmem=1024
 coretype=m1.medium
-corecount=49
+corecount=1
 tasktype=m1.medium
-taskcount=49
+taskcount=1
 optmem="-m,mapred.reduce.child.java.opts=-Xmx${maxmem}m"
 
-elastic-mapreduce --create --enable-debugging --instance-group master --instance-type m1.medium --instance-count 1 --instance-group core --instance-type ${coretype} --instance-count ${corecount}  --instance-group task --instance-type ${tasktype}  --instance-count ${taskcount} --name "partitionrunner.${corecount}.${taskcount}"  --bootstrap-action 's3://aaji/scratch/awsjoin/bootcopy.sh' --bootstrap-action s3://elasticmapreduce/bootstrap-actions/configure-hadoop --args "-m,mapred.tasktracker.reduce.tasks.maximum=${maxred}" --region 'us-east-1' --log-uri 's3://aaji/scratch/logs' --key-pair aaji --with-termination-protection 
+elastic-mapreduce --create --alive --enable-debugging --instance-group master --instance-type m1.medium --instance-count 1 --instance-group core --instance-type ${coretype} --instance-count ${corecount}  --instance-group task --instance-type ${tasktype}  --instance-count ${taskcount} --name "ppart.${corecount}.${taskcount}" --bootstrap-action 's3://aaji/scratch/awsjoin/bootcopy.sh' --region 'us-east-1' --log-uri 's3://aaji/scratch/logs' --key-pair aaji --with-termination-protection 
+
+# --bootstrap-action s3://elasticmapreduce/bootstrap-actions/configure-hadoop --args "-m,mapred.tasktracker.reduce.tasks.maximum=${maxred}" 
 
 
 # Wait for previous step to complete
