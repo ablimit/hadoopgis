6a29c3f7a3858d9c83fbac20b40d534e4ab1d170
Ablimit Aji <ablimit@gmail.com>
2014-02-25 13:24:55 -0500

    accomodate missing records

--- partition/mappartition.py
+++ partition/mappartition.py
@@ -4,28 +4,31 @@ import sys
 dic = {}
 
 def readstdin():
-  global dic
-  for line in sys.stdin:
-    line = line.strip()
-    sp =line.split()
-    if len(sp)>1:
-      dic[int(sp[0])] = line
+    global dic
+    for line in sys.stdin:
+        line = line.strip()
+        sp =line.split()
+        if len(sp)>1:
+            dic[int(sp[0])] = line
 
 def main():
-  global dic
-  if len(sys.argv) < 2:
-    sys.stderr.write("Usage: "+ sys.argv[0] + "\n")
-  
-  readstdin()
-  
-  for line in open(sys.argv[1],'r'):
-    sp =line.strip().split()
-    pid = sp[0]
-    for oid in set([int(item) for item in sp[1:]]):
-      print " ".join((pid,dic[oid]))
-  
-  sys.stdout.flush()
+    global dic
+    if len(sys.argv) < 2:
+        sys.stderr.write("Usage: "+ sys.argv[0] + "\n")
+    readstdin()
+    i = 0
+    for line in open(sys.argv[1],'r'):
+        sp =line.strip().split()
+        pid = sp[0]
+        for oid in set([int(item) for item in sp[1:]]):
+            if oid in dic:
+                print " ".join((pid,dic[oid]))
+            else:
+                i +=1
+    sys.stdout.flush()
+    sys.stderr.write(' '.join("missing records:",str(i),"\n"))
+    sys.stderr.flush()
 
 if __name__ == '__main__':
-  main()
+    main()
 
