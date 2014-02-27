efde0f58c941fc098dc8f0575f7ebaa56496c614
Ablimit Aji <aaji@mathcs.emory.edu>
2014-02-27 02:54:25 -0500

    plot mapreduce partition scalability

--- partition/plots/mapred/scale/extract.sh
+++ partition/plots/mapred/scale/extract.sh
@@ -2,9 +2,10 @@
 
 for i in 6 12 25 50
 do
-  for d in 5 10 20 50 86
+  #for d in 5 10 20 50 86
+  for d in 50
   do
-    echo -n "${d}0000 " >> levels.dat
+    echo -n "${i} " > levels.${i}.dat
     cat osm.${i}node.csv | grep ,${d}0000, | cut -d, -f1,4 | python gendata.py >> levels.${i}.dat
     #cat ../osm.csv | grep ,${d}0000, | grep slc  | cut -d, -f3,4 | sort -t, -nk1  > slc.dat
     #cat ../osm.csv | grep ,${d}0000, | grep bos  | cut -d, -f3,4 | sort -t, -nk1 | cut -d, -f2 > bos.dat
