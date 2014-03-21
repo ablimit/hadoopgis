9214b6c8ea35daa5e3805f1fc0f16f9e42c82e76
Ablimit Aji <ablimit@gmail.com>
2014-03-20 23:32:50 -0400

    boost spatial lib

--- gpu/mapred/cpu/boost/polyclip.cpp
+++ gpu/mapred/cpu/boost/polyclip.cpp
@@ -1,69 +1,20 @@
-#include <stdlib.h>
-#include <stdio.h>
+// #include <stdlib.h>
+//#include <stdio.h>
 #include "cpu_spatial.h"
 
-// compute whether point (x, y) lays within a polygon whose contour
-// is specified by xs and ys in the range of [istart, iend); the last
-// vertex repeats the first vertex
-static int in_poly(
-    const int x, const int y,
-    const int *xs, const int *ys,
-    const int start, const int end)
-{
-  // Count the number of intersections between the horizontal beam from (x,y) to right infinity
-  // and the edges of the polygon. Odd number indicates the point lies within the polygon.
-  int crossing = 0;
-  for(int i = start; i < end-1; i++)
-  {
-    int startx = xs[i], starty = ys[i];
-    int endx = xs[i+1], endy = ys[i+1];
+#include <boost/foreach.hpp>
+#include <boost/geometry.hpp>
+#include <boost/geometry/geometry.hpp>
+#include <boost/geometry/geometries/point_xy.hpp>
+#include <boost/geometry/geometries/polygon.hpp>
 
-    // A horizontal edge? Then no crossing.
-    if(endy == starty)
-    {
-      continue;
-    }
+using namespace std;
+//using namespace boost;
 
-    // If this is an upword edge
-    if(endy > starty)
-    {
-      if(y == starty && x <= startx) {
-        crossing++;
-      }
-      /*			if(y == starty && x <= startx)
-              {
-              crossing++;
-              }
-              else if(y >= starty && y < endy)
-              {
-              if(x < startx + (y - starty) * (endx - startx) * 1.0 / (endy - starty))
-              {
-              crossing++;
-              }
-              }
-              */		}
-      // If this is a downward edge
-    else
-    {
-      if(y == endy && x <= endx) {
-        crossing++;
-      }
-      /*			if(y == endy && x <= endx)
-              {
-              crossing++;
-              }
-              else if(y >= endy && y < starty)
-              {
-              if(x < startx + (starty - y) * (endx - startx) * 1.0 / (starty - endy))
-              {
-              crossing++;
-              }
-              }
-              */		}
-  }
+// data type declaration 
+typedef boost::geometry::model::d2::point_xy<int> point;
+typedef boost::geometry::model::polygon<point> polygon;
 
-  return crossing & 1;
-}
 
 float *clip(
     const int nr_poly_pairs, // mbr of each poly pair
@@ -83,24 +34,29 @@ float *clip(
   }
 
   for (size_t i = 0 ; i< nr_poly_pairs ; i++){
-    int area_union = 0 ; 
-    int area_inter = 0 ; 
-    int inp1, inp2, x, y;
+    float area_union = 0.0 ; 
+    float area_inter = 0.0 ; 
+    polygon p; polygon q;
 
-    size_t nr_pixels = (size_t)(mbrs[i].r - mbrs[i].l + 1) * (mbrs[i].t - mbrs[i].b + 1);
+    for (int j = offsets1[idx1[i]]; j < offsets1[idx1[i]+1] - 1; j++ )
+      p.outer().push_back(point(x1[j], y1[j]));
+    for (int j = offsets2[idx2[i]]; j < offsets2[idx2[i]+1] - 1; j++ )
+      q.outer().push_back(point(x2[j], y2[j]));
 
-    for(size_t ipoint = 0; ipoint < nr_pixels; ipoint++)
+    std::vector<polygon> output;
+    boost::geometry::intersection(p,q, output);
+    BOOST_FOREACH(polygon const& a, output)
     {
-      x = ipoint % (mbrs[i].r - mbrs[i].l + 1) + mbrs[i].l;
-      y = ipoint / (mbrs[i].t - mbrs[i].b + 1) + mbrs[i].b;
-
-      inp1 = in_poly(x, y, x1, y1, offsets1[idx1[i]], offsets1[idx1[i]+1]);	// in poly1?
-      inp2 = in_poly(x, y, x2, y2, offsets2[idx2[i]], offsets2[idx2[i]+1]);	// in poly2?
-      area_inter += inp1 & inp2;
-      area_union += inp1 | inp2;
+      area_inter += boost::geometry::area(a) ;
+    }
+    if (output.size()==0) 
+    {
+      ratios[i] = 0.0; 
+      continue ;
     }
 
-    ratios[i] = area_inter / (float)area_union;
+    area_union = boost::geometry::area(p) + boost::geometry::area(q) - area_inter ; 
+    ratios[i] = area_inter / area_union;
   }
 
   return ratios;
