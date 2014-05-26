c922a24dd1622ffa842a5867295d45b1488feef8
Ablimit Aji <ablimit@gmail.com>
2014-05-25 19:11:04 -0700

    single node scheduler code

---
+++ gpu/mapred/snscheduler/TaskId.cpp
@@ -0,0 +1,30 @@
+#include "TaskId.h"
+
+TaskId::TaskId() {
+}
+
+TaskId::~TaskId() {
+  std::cout << "~TaskId" << std::endl;
+}
+
+bool TaskId::run(int procType, int tid)
+{
+  if (procType == ExecEngineConstants::CPU) {
+    sleep(2);
+    std::cout << "executing on the CPU engine. " << std::endl;
+
+  }
+  else if( procType == ExecEngineConstants::GPU ) {
+
+    sleep(1);
+    std::cout << "executing on the GPU engine. " << std::endl;
+
+    //sleep(5/this->getSpeedup(ExecEngineConstants::GPU));
+  }
+  else 
+    std::cout << "No idea how to handle. " << std::endl;
+  //	std::cout << "Task.id = "<< this->getId() << std::endl;
+  this->printDependencies();
+  return true;
+}
+
