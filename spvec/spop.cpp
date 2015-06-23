1deb696faa4ca23d75f6f228162b8f20b079bf15
Ablimit Aji <abulimiti.aji@hp.com>
2015-06-22 18:34:03 -0700

    SOA vs. AOS

--- spvec/spop.cpp
+++ spvec/spop.cpp
@@ -2,29 +2,62 @@
 #include <stdlib.h>
 
 #include "spop_ispc.h"
-#include "spvec.h"
+//#include "spvec.h"
 #include "timing.h"
 
-extern double polygon[1016];
-extern double ptarray_signed_area(double pa[] , const int npoints);
-const int npoints =1015;
+double polygon_aos[12] = {3.0,4.0,5.0,11.0,12.0,8.0,9.0,5.0,5.0,6.0,3.0,4.0};
+double polygon_soa[12] = {3.0,5.0,12.0,9.0,5.0,3.0,4.0,11.0,8.0,5.0,6.0,4.0};
+extern double ptarray_signed_area_aos(double pa[] , const int npoints);
+extern double ptarray_signed_area_soa(double pa[] , const int npoints);
+//const int npoints =1014;
+const int npoints =10;
 
 //using namespace ispc;
 
 int main(int argc, char** argv) {
-        double area[2];
-        reset_and_start_timer();
-        area[0]= ptarray_signed_area(polygon,npoints);
-        double aTime = get_elapsed_mcycles();
+  double area[2];
+  double aTime = 0.0;
+  double bTime = 0.0;
+  
+  int reps = argc > 1 ? atoi(argv[1]) :100;
 
-        reset_and_start_timer();
-        area[1]= ispc::ptarray_signed_area(polygon,npoints);
-        double bTime = get_elapsed_mcycles();
+  printf("ProgramCount: %d\n",ispc::get_programCount());
+  
+    for (int i =0; i<reps; i++) {
+    reset_and_start_timer();
+    area[0]= ptarray_signed_area_aos(polygon_aos,npoints);
+    aTime += get_elapsed_mcycles();
+  }
 
-        printf("%-40s: [%.2f] M cycles %s, [%.2f] M cycles %s (%.2fx speedup).\n",
-               "ST_AREA", aTime, "serial", bTime, "ispc",
-               aTime/bTime);
-        printf("serial [%.2f], ispc [%.2f]\n", area[0], area[1]);
-    
-        return 0;
+  for (int i =0; i<reps; i++) {
+    reset_and_start_timer();
+    area[1]= ispc::ptarray_signed_area_aos(polygon_aos,npoints);
+    bTime += get_elapsed_mcycles();
+  }
+
+  printf("%-20s: [%.2f] M cycles %s, [%.2f] M cycles %s (%.2fx speedup).\n",
+      "ST_AREA", aTime, "serial_aos", bTime, "ispc_aos",
+      aTime/bTime);
+  printf("%-20s: serial [%.2f], ispc [%.2f]\n", "results", area[0], area[1]);
+
+
+  aTime = 0.0;
+  bTime = 0.0;
+  for (int i =0; i<reps; i++) {
+    reset_and_start_timer();
+    area[0]= ptarray_signed_area_soa(polygon_soa,npoints);
+    aTime += get_elapsed_mcycles();
+  }
+  
+  for (int i =0; i<reps; i++) {
+    reset_and_start_timer();
+    area[1]= ispc::ptarray_signed_area_soa(polygon_soa,npoints);
+    bTime += get_elapsed_mcycles();
+  }
+  printf("%-20s: [%.2f] M cycles %s, [%.2f] M cycles %s (%.2fx speedup).\n",
+      "ST_AREA", aTime, "serial_soa", bTime, "ispc_soa",
+      aTime/bTime);
+  printf("serial [%.2f], ispc [%.2f]\n", area[0], area[1]);
+
+  return 0;
 }
--- spvec/spop_serial.cpp
+++ spvec/spop_serial.cpp
@@ -3,8 +3,9 @@
 * positive if it is oriented CW and zero if the ring is degenerate or flat.
 * http://en.wikipedia.org/wiki/Shoelace_formula
 */
+#include<stdio.h>
 
-double ptarray_signed_area(double pa[] , const int npoints)
+double ptarray_signed_area_aos(double pa[] , const int npoints)
 {
 	if (! pa || npoints < 3 )
 		return 0.0;
@@ -14,9 +15,35 @@ double ptarray_signed_area(double pa[] , const int npoints)
 
 	for(int i= 0 ; i< npoints; i+=2) {
 		sum += (pa[i] * pa[i+3] - pa[i+1] *pa[i+2]);
+    //printf("coef %d : %f\n", i, a);
 	}
 
 	return sum * 0.5;	
 }
 
+
+double ptarray_signed_area_soa(double pa[] , const int npoints)
+{
+	if (! pa || npoints < 3 )
+		return 0.0;
+
+  double sum = 0.0;
+  double * xp = pa;
+  
+  for(int i= 0 ; i<npoints/16; i++, xp+=16) {
+    for(int j= 0 ; j<8; j++) {
+      //printf("+ %f : -%f\n", a, b);
+      sum += (xp[j] * xp[j+9] - xp[j+1] *xp[j+8]);
+    }
+  }
+
+  int remain = (npoints % 16)/2; 
+  //printf("remain %d\n", remain);
+  for(int j= 0 ; j<remain; j++) {
+    //printf("+ %f : -%f\n", a, b);
+    sum += (xp[j] * xp[j+remain+2] - xp[j+1] *xp[j+remain+1]);
+  }
+  //printf("sum: %f\n", sum);
+  return sum * 0.5;	
+}
 
