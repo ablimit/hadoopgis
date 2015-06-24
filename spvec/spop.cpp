2881c5f24448db6566fcb6dd87bed46dc4627aa4
Ablimit Aji <ablimit@gmail.com>
2015-06-24 02:20:55 -0700

    incremental change

--- spvec/spop.cpp
+++ spvec/spop.cpp
@@ -2,15 +2,15 @@
 #include <stdlib.h>
 
 #include "spop_ispc.h"
-//#include "spvec.h"
+#include "spvec.h"
 #include "timing.h"
 
-double polygon_aos[12] = {3.0,4.0,5.0,11.0,12.0,8.0,9.0,5.0,5.0,6.0,3.0,4.0};
-double polygon_soa[12] = {3.0,5.0,12.0,9.0,5.0,3.0,4.0,11.0,8.0,5.0,6.0,4.0};
+//double polygon_aos[12] = {3.0,4.0,5.0,11.0,12.0,8.0,9.0,5.0,5.0,6.0,3.0,4.0};
+//double polygon_soa[12] = {3.0,5.0,12.0,9.0,5.0,3.0,4.0,11.0,8.0,5.0,6.0,4.0};
 extern double ptarray_signed_area_aos(double pa[] , const int npoints);
 extern double ptarray_signed_area_soa(double pa[] , const int npoints);
-//const int npoints =1014;
-const int npoints =10;
+const int npoints =1014;
+//const int npoints =10;
 
 //using namespace ispc;
 
@@ -48,16 +48,16 @@ int main(int argc, char** argv) {
     area[0]= ptarray_signed_area_soa(polygon_soa,npoints);
     aTime += get_elapsed_mcycles();
   }
-  
+ /* 
   for (int i =0; i<reps; i++) {
     reset_and_start_timer();
     area[1]= ispc::ptarray_signed_area_soa(polygon_soa,npoints);
     bTime += get_elapsed_mcycles();
-  }
+  }*/
   printf("%-20s: [%.2f] M cycles %s, [%.2f] M cycles %s (%.2fx speedup).\n",
       "ST_AREA", aTime, "serial_soa", bTime, "ispc_soa",
       aTime/bTime);
-  printf("serial [%.2f], ispc [%.2f]\n", area[0], area[1]);
+  printf("%-20s: serial [%.2f], ispc [%.2f]\n", "results", area[0], area[1]);
 
   return 0;
 }
--- spvec/spop_serial.cpp
+++ spvec/spop_serial.cpp
@@ -14,7 +14,13 @@ double ptarray_signed_area_aos(double pa[] , const int npoints)
 	//assert(programCount <= 8);
 
 	for(int i= 0 ; i< npoints; i+=2) {
-		sum += (pa[i] * pa[i+3] - pa[i+1] *pa[i+2]);
+			double x0 = pa[i];
+			double x1 = pa[i+2];
+			double y0 = pa[i + 1];
+			double y1 = pa[i+3];
+			sum += (x0*y1 - x1*y0); 
+      //printf("%f : %f\n", x0*y1, x1*y0);
+		//sum += (pa[i] * pa[i+3] - pa[i+1] *pa[i+2]);
     //printf("coef %d : %f\n", i, a);
 	}
 
@@ -29,19 +35,26 @@ double ptarray_signed_area_soa(double pa[] , const int npoints)
 
   double sum = 0.0;
   double * xp = pa;
+  double * np = xp+16;
+  int iter = npoints/16; 
+  int remain = (npoints % 16)/2;
+  int dist = remain +1 ;
   
-  for(int i= 0 ; i<npoints/16; i++, xp+=16) {
+  for(int i= 0 ; i<iter; i++, xp+=16, np+=16) {
     for(int j= 0 ; j<8; j++) {
-      //printf("+ %f : -%f\n", a, b);
-      sum += (xp[j] * xp[j+9] - xp[j+1] *xp[j+8]);
+			double x0 = xp[j];
+			double x1 = j==7 ? np[0] : xp[j + 1];
+			double y0 = xp[8 + j ];
+			double y1 = j==7 ? np[i+1< iter ? 8 : dist] : xp[8 + j + 1];
+			sum += (x0*y1 - x1*y0); 
+      //printf("%f : %f\n", x0*y1, x1*y0);
+      //sum += (xp[j] * xp[j+9] - xp[j+1] *xp[j+8]);
     }
   }
 
-  int remain = (npoints % 16)/2; 
-  //printf("remain %d\n", remain);
   for(int j= 0 ; j<remain; j++) {
     //printf("+ %f : -%f\n", a, b);
-    sum += (xp[j] * xp[j+remain+2] - xp[j+1] *xp[j+remain+1]);
+    sum += (xp[j] * xp[j+dist+1] - xp[j+1] *xp[j+dist]);
   }
   //printf("sum: %f\n", sum);
   return sum * 0.5;	
