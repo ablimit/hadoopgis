c40a17e22ec369aef9b7d7b7585609fef2a5565a
Ablimit Aji <ablimit@gmail.com>
2015-07-20 02:12:07 -0700

    mac change

--- spvec/spop.cpp
+++ spvec/spop.cpp
@@ -5,23 +5,23 @@
 #include "spvec.h"
 #include "timing.h"
 
-//double polygon_aos[12] = {3.0,4.0,5.0,11.0,12.0,8.0,9.0,5.0,5.0,6.0,3.0,4.0};
-//double polygon_soa[12] = {3.0,5.0,12.0,9.0,5.0,3.0,4.0,11.0,8.0,5.0,6.0,4.0};
-extern double ptarray_signed_area_aos(double pa[] , const int npoints);
-extern double ptarray_signed_area_soa(double pa[] , const int npoints);
+//float polygon_aos[12] = {3.0,4.0,5.0,11.0,12.0,8.0,9.0,5.0,5.0,6.0,3.0,4.0};
+//float polygon_soa[12] = {3.0,5.0,12.0,9.0,5.0,3.0,4.0,11.0,8.0,5.0,6.0,4.0};
+extern float ptarray_signed_area_aos(float pa[] , const int npoints);
+extern float ptarray_signed_area_soa(float pa[] , const int npoints);
 const int npoints =1014;
 //const int npoints =10;
 
 //using namespace ispc;
 
 int main(int argc, char** argv) {
-  double area[2];
-  double aTime = 0.0;
-  double bTime = 0.0;
+  float area[2];
+  float aTime = 0.0;
+  float bTime = 0.0;
   
   int reps = argc > 1 ? atoi(argv[1]) :100;
 
-  printf("ProgramCount: %d\n",ispc::get_programCount());
+  printf("ProgramCount [%d], size-of-float[%lu], size-of-float[%lu]\n",ispc::get_programCount(),sizeof(float), sizeof(float));
   
     for (int i =0; i<reps; i++) {
     reset_and_start_timer();
@@ -48,12 +48,12 @@ int main(int argc, char** argv) {
     area[0]= ptarray_signed_area_soa(polygon_soa,npoints);
     aTime += get_elapsed_mcycles();
   }
- /* 
+ 
   for (int i =0; i<reps; i++) {
     reset_and_start_timer();
-    area[1]= ispc::ptarray_signed_area_soa(polygon_soa,npoints);
+    area[1]= ispc::ptarray_signed_area_soa(polygon_aos,npoints);
     bTime += get_elapsed_mcycles();
-  }*/
+  }
   printf("%-20s: [%.2f] M cycles %s, [%.2f] M cycles %s (%.2fx speedup).\n",
       "ST_AREA", aTime, "serial_soa", bTime, "ispc_soa",
       aTime/bTime);
